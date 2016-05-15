c0f90c74c155fe105b875ea58bea4c1251d7ce65
Patricio Bruna <pbruna@gmail.com>
2016-05-15 13:50:18 -0400

    Ready to be used. Documentation updated

---
+++ README.md
@@ -0,0 +1,202 @@
+# PowerDNS API Javascript Wrapper
+
+This is a Wrapper for the [PowerDNS REST API](https://doc.powerdns.com/3/httpapi/README/#api-specification).
+
+### Important NGINX
+Given a [Bug](https://github.com/PowerDNS/pdns/issues/3723) on PowerDNS, we are using NGINX
+to proxy the request to PowerDNS.
+
+Following is an example configuration for NGINX:
+
+```
+upstream pdns {
+   server localhost:8091;
+}
+
+server {
+  listen  8081;
+  server_name pdns;
+
+  location / {
+    proxy_set_header Host $http_host;
+    proxy_pass http://pdns/;
+    proxy_buffering off;
+    proxy_read_timeout 300s;
+    gzip off;
+  }
+
+  client_max_body_size 4G;
+  keepalive_timeout 10;
+}
+```
+
+## Table of Contents
+- [Example](#example)
+- [Install](#install)
+- [Callback](#callback)
+- [Errors](#errors)
+- [Common Functions](#common-functions)
+
+
+## Example
+
+First, instantiate the wrapper.
+```javascript
+const jsPowerdns = require('js-powerdns');
+const api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+
+var callback = function(err, data) {
+  if (err) return console.log(err);
+  console.log(data);
+};
+
+// Get all the Zones
+api.getZones(callback);
+// [Zone, Zone, Zone]
+
+jsPowerdns.version();
+// "0.0.1"
+```
+
+## Install
+node:
+
+```
+$ npm install js-powerdns
+```
+
+webpack:
+
+```
+TODO
+```
+
+## Callback
+You have to pass a `callback` to all the functions, that receives to params:
+
+1. `error`, if any
+2. `data`, if any
+
+For this documentations `callback` will always be:
+
+```javascript
+function(err, data) {
+  if (err) return console.log(err);
+  console.log(data);
+};
+```
+
+## Errors
+
+If an error happens, the library return an Object:
+
+```javascript
+Error {
+  status: 401 // HTTP result Code,
+  reason: 'Unauthorized' // Text error description
+}
+```
+
+## Common Functions
+### Get all Zones
+
+```javascript
+api.getZones(callback);
+// [Zone, Zone...]
+```
+
+### Get a Zone
+
+**Important**: you have to use the zone name with a `.` at the end of it. For example if the zone name is `example.com`, you **must** use `example.com.`.
+
+```javascript
+api.getZone("example.com.", callback);
+// Zone {
+//  url: '/servers/localhost/zones/example.com.',
+//  id: 'example.com.',
+//  name: 'example.com.',
+//  dnssec: false,
+//  account: '',
+//  masters: [],
+//  records: undefined,
+// }
+```
+
+### Create a Zone
+
+```javascript
+const zone_object = { name: 'example.org', kind: 'Master', nameservers: [] };
+api.createZone(zone_object, callback);
+// Zone {}
+```
+
+### Create a Zone with Records
+
+```javascript
+// New Zone object
+const zone_object = { name: 'example.org', kind: 'Master', nameservers: [] };
+
+// New Records objects
+const records = [];
+const record1 = { "name": "record1.example.org", "content": "192.0.5.1",
+                 "disabled": false, "ttl": 86400, "type": "A"
+               };
+const record2 = { "name": "record2.example.org", "content": "192.0.5.1",
+                "disabled": false, "ttl": 86400, "type": "A"
+              };
+records.push(record1);
+records.push(record2);
+
+api.createZoneWithRecords(zone_data, records, callback);
+// Zone {}
+```
+
+### Delete Zone
+
+```javascript
+api.deleteZone("example.com.", callback);
+// {}
+```
+
+### Delete Records
+
+You need to use an object with `name` and `content` attributes to reference the Record.
+
+```javascript
+// Record to delete
+const record = { "name": "record1.example.org", "content": "192.0.5.1" }
+
+// deleteRecords(zone_url, records_to_be_deleted, callback)
+api.deleteRecords('/servers/localhost/zones/example.org.', record, callback)
+
+// Zone {} with records deleted
+```
+
+If you have a `Zone` object you can use:
+
+```javascript
+// Record to delete
+const record = { "name": "record1.example.org", "content": "192.0.5.1" }
+
+// zone is a Zone {}
+zone.deleteRecords(record, callback);
+```
+
+### Create or Modify Records
+
+The function `createOrModifyRecords` update the Record information if there is a match for the `content` and `name` of the record object.
+
+If no match, a new Record is created.
+
+```javascript
+// Record to add
+const record = { "name": "record1.example.org", "content": "192.0.5.1" }
+
+// Add new Record
+zone.modifyOrCreateRecords(record, callback);
+
+record.content = '1.1.1.2';
+
+// Modify Record
+zone.modifyOrCreateRecords(record, callback);
+```
---
+++ test/test.js
@@ -0,0 +1,195 @@
+"use strict";
+
+var requireHelper = require('./require_helper'),
+    jsPowerdns = requireHelper('index.js'),
+    expect = require('chai').expect,
+    superagent = require('superagent');
+
+(function() {
+
+
+  describe('Client Api', function() {
+
+   it('should return error if wrong token', function(done){
+     let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'bad' });
+     api.client.getRequest({path: '/servers/localhost/zones'}, function(err, data){
+       expect(err.status).to.be.equal(401);
+       expect(err.reason).to.be.equal('Unauthorized');
+       done();
+     });
+   });
+  });
+
+  describe('Getting zones', function() {
+
+    it('should get the zones', function(done){
+      let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+      api.getZones(function(err,data){
+        if (err) return console.error(err);
+        console.log(data);
+        expect(data[0].constructor.name).to.be.equal('Zone')
+        done();
+      });
+    });
+
+    it('should get the zone', function(done){
+      let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+      const zone_name = 'example.org.';
+      api.getZone(zone_name, function(err,data){
+        if (err) return console.error(err);
+        expect(data.constructor.name).to.be.equal('Zone')
+        expect(data.name).to.be.equal(zone_name)
+        done();
+      });
+    });
+
+    it('should return error if the zone does not exists', function(done){
+      let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+      const zone_name = 'god.org.';
+      api.getZone(zone_name, function(err,data){
+        expect(err.status).to.be.equal(422);
+        done();
+      });
+    });
+  });
+
+  describe('Creating Zones', function() {
+
+    it('should create the Zone', function(done){
+      let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+      const zone_name = Date.now() + '.com.';
+      const zone_data = { name: zone_name, kind: 'Master', nameservers: [] }
+      api.createZone(zone_data, function(err,data){
+        if (err) return console.error(err);
+        api.getZone(zone_name, function(err, data){
+          if (err) return console.error(err);
+          expect(data.name).to.be.equal(zone_name);
+          done();
+        });
+      });
+    });
+
+    it('should return error without Nameservers', function(done){
+      let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+      const zone_name = Date.now() + '.com.';
+      const zone_data = { name: zone_name, kind: 'Master' }
+      api.createZone(zone_data, function(err,data){
+        expect(err.status).to.be.equal(422);
+        expect(err.reason).to.be.equal('Nameservers list must be given (but can be empty if NS records are supplied)');
+        done();
+      });
+    });
+
+    it('should create the Zone with Records', function(done){
+      let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+      const zone_name = Date.now() + '.com.';
+      const record_name = `test.${zone_name.replace(/\.$/, '')}`;
+      const record_name2 = `test2.${zone_name.replace(/\.$/, '')}`;
+      const records = [ {"content": "192.0.5.4", "disabled": false, "name": record_name, "ttl": 86400, "type": "A" } ];
+      records.push({ "content": "192.0.5.5", "disabled": false, "name": record_name2, "ttl": 86400, "type": "A" });
+      const zone_data = { name: zone_name, kind: 'Master', nameservers: []};
+      api.createZoneWithRecords(zone_data, records, function(err,data){
+        if (err) return console.error(err);
+        expect(data.records[1].name).to.be.equal(record_name);
+        expect(data.records[2].content).to.be.equal('192.0.5.5');
+        done();
+      });
+    });
+
+    it('should delete the Zone', function(done){
+      let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+      const zone_name = Date.now() + '.com.';
+      const zone_data = { name: zone_name, kind: 'Master', nameservers: [] }
+      api.createZone(zone_data, function(err,data){
+        if (err) return console.error(err);
+        const zone = data;
+        api.deleteZone(zone.url, function(err,data){
+          expect(JSON.stringify(data)).to.be.equal('{}');
+          done();
+        });
+      });
+    });
+
+
+  });
+
+  describe('Temp Record', function() {
+    var tmp_zone = null;
+
+    beforeEach(function(done) {
+      let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+      const zone_name = 'tempdomain.com.';
+      const record_name = `test.${zone_name.replace(/\.$/, '')}`;
+      const record_name2 = `test2.${zone_name.replace(/\.$/, '')}`;
+      const records = [ {"content": "192.0.5.4", "disabled": false, "name": record_name, "ttl": 86400, "type": "A" } ];
+      records.push({ "content": "192.0.5.5", "disabled": false, "name": record_name2, "ttl": 86400, "type": "A" });
+      const zone_data = { name: zone_name, kind: 'Master', nameservers: []};
+      api.createZoneWithRecords(zone_data, records, done);
+    });
+
+    afterEach(function(done) {
+      let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+      const zone_name = 'tempdomain.com.';
+      api.deleteZone('/servers/localhost/zones/' + zone_name, done);
+    });
+
+    describe('Managing Zone', function() {
+
+      it('Should delete record from Zone', function(done){
+        let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+        api.getZone('tempdomain.com.', function(err, tmp_zone){
+          const org_records_size = tmp_zone.records.length;
+          const record = tmp_zone.records[1];
+          tmp_zone.deleteRecords(record, function(err, zone){
+            if (err) return console.error(err);
+            expect(zone.records.length).to.be.below(org_records_size);
+            done();
+          });
+        });
+      });
+
+      it('Should modify record from Zone', function(done){
+        let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+        api.getZone('tempdomain.com.', function(err, tmp_zone){
+          const last_record = tmp_zone.records[2];
+          last_record.content = '1.1.1.1';
+          tmp_zone.createOrModifyRecords(last_record, function(err, zone){
+            if (err) return console.error(err);
+            expect(zone.records.length).to.be.equal(3);
+            expect(zone.records[2].content).to.be.equal('1.1.1.1');
+            done();
+          });
+        });
+      });
+
+      it('Should return error if wrong data', function(done){
+        let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+        api.getZone('tempdomain.com.', function(err, tmp_zone){
+          const last_record = tmp_zone.records[2];
+          last_record.content = 'pdmdod';
+          tmp_zone.createOrModifyRecords(last_record, function(err, zone){
+            expect(err.status).to.be.above(400);
+            expect(err.reason).to.match(/Parsing/);
+            done();
+          });
+        });
+      });
+
+      it('Should add new Record to Zone', function(done){
+        let api = new jsPowerdns({ url: 'http://127.0.0.1:8081', token: 'otto' });
+        const new_record = {"content": "192.0.5.44", "disabled": false, "name": 'temp3.tempdomain.com', "ttl": 86400, "type": "A" };
+        api.getZone('tempdomain.com.', function(err, tmp_zone){
+          const org_records_size = tmp_zone.records.length;
+          tmp_zone.createOrModifyRecords(new_record, function(err, zone){
+            if (err) return console.error(err);
+            expect(zone.records.length).to.be.above(org_records_size);
+            done();
+          });
+        });
+      });
+
+    });
+
+  });
+
+})();
